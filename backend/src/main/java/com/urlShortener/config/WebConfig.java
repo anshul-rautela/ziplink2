9296bdc8206a0fc54f3e2c9fcf23dcb6e969b875
Anshul Rautela <anshulrautela123456789@gmail.com>
2026-01-30 00:04:13 +0530

    Update CORS configuration to allow all origins

--- backend/src/main/java/com/urlShortener/config/WebConfig.java
+++ backend/src/main/java/com/urlShortener/config/WebConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+         registry.addMapping("/**")
+                .allowedOrigins("*")  // Allows all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
